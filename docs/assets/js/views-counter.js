f0056465774e28919e301982f310ef5e6ee9edd2
nyarkus <nyarkus@icloud.com>
2025-07-31 15:57:09 +0300

    change backend name

--- docs/assets/js/views-counter.js
+++ docs/assets/js/views-counter.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
   if (!path) return;
 
-  const workerUrl = "https://linux-wiki-backend.nyarkus.workers.dev?path=" + encodeURIComponent(path);
+  const workerUrl = "https://lsg-backend.nyarkus.workers.dev?path=" + encodeURIComponent(path);
 
   fetch(workerUrl, {
     method: "POST",
